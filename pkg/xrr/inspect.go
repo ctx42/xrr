715c62e4804fe150a102054ba31adc06add4ba8f
Rafal Zajac <rzajac@gmail.com>
2025-09-06 20:58:53 +0200

    feat: Implement `xrr.GetMeta` helper.

--- pkg/xrr/inspect.go
+++ pkg/xrr/inspect.go
@@ -32,3 +32,46 @@ func GetCode(err error) string {
 	}
 	return ECGeneric
 }
+
+// GetMeta recursively retrieves metadata from an error and its wrapped errors.
+//
+// It returns a map containing metadata from the error, merging metadata in a
+// top-down order (metadata from the outermost error takes precedence). If any
+// of the errors implements the MetaAll method, it's called to get its metadata.
+// For errors implementing Unwrap() []error, metadata is collected from all
+// wrapped errors, prioritizing earlier errors in the sequence over later ones.
+// Returns nil if no metadata is found or the error is nil.
+func GetMeta(err error) map[string]any {
+	if err == nil || isNil(err) {
+		return nil
+	}
+
+	switch e := err.(type) { // nolint: errorlint
+	case interface{ MetaAll() map[string]any }:
+		hi := e.MetaAll()
+		lo := GetMeta(errors.Unwrap(err))
+		if len(lo) == 0 {
+			return hi
+		}
+		for k, v := range hi {
+			lo[k] = v
+		}
+		return lo
+
+	case interface{ Unwrap() []error }:
+		var meta map[string]any
+		ers := e.Unwrap()
+		for i := len(ers) - 1; i >= 0; i-- {
+			if m := GetMeta(ers[i]); m != nil {
+				if meta == nil {
+					meta = make(map[string]any, len(m))
+				}
+				for k, v := range m {
+					meta[k] = v
+				}
+			}
+		}
+		return meta
+	}
+	return GetMeta(errors.Unwrap(err))
+}
--- pkg/xrr/inspect_test.go
+++ pkg/xrr/inspect_test.go
@@ -79,3 +79,115 @@ func Test_GetCode_tabular(t *testing.T) {
 		})
 	}
 }
+
+func Test_GetMeta_tabular(t *testing.T) {
+	var err error
+
+	tt := []struct {
+		testN string
+
+		err  error
+		want map[string]any
+	}{
+		{"nil", nil, nil},
+		{"nil typed", err, nil},
+		{"std error", errors.New("msg"), nil},
+		{"xrr without metadata", New("msg", "ECode"), nil},
+		{
+			"xrr with metadata",
+			New("msg", "ECode", Meta().Int("A", 1).Bool("B", true).Option()),
+			map[string]any{"A": 1, "B": true},
+		},
+		{
+			"xrr std wrapped with metadata",
+			fmt.Errorf(
+				"wrapped: %w",
+				New("msg", "ECode", Meta().Int("A", 1).Bool("B", true).Option()),
+			),
+			map[string]any{"A": 1, "B": true},
+		},
+		{
+			"xrr std wrapped multiple times with metadata",
+			fmt.Errorf(
+				"second: %w",
+				fmt.Errorf(
+					"first: %w",
+					New("msg", "ECode", Meta().Int("A", 1).Bool("B", true).Option()),
+				),
+			),
+			map[string]any{"A": 1, "B": true},
+		},
+		{
+			"joined errors",
+			errors.Join(
+				New("msg", "ECode", Meta().Int("A", 1).Int("B", 1).Option()),
+				New("msg", "ECode", Meta().Int("A", 1).Int("B", 2).Option()),
+			),
+			map[string]any{"A": 1, "B": 1},
+		},
+		{
+			"joined and std wrapped errors",
+			errors.Join(
+				New("msg", "ECode", Meta().Int("A", 1).Int("B", 1).Option()),
+				New("msg", "ECode", Meta().Int("A", 2).Int("B", 2).Option()),
+				fmt.Errorf(
+					"wrapped: %w",
+					New("msg", "ECode", Meta().Int("A", 3).Int("C", 1).Option()),
+				),
+			),
+			map[string]any{"A": 1, "B": 1, "C": 1},
+		},
+		//      A7,B0
+		//        │
+		//        │
+		//   ┌──A6,C1───┐
+		//   │          │
+		//   │          │
+		// A4,D2    ┌─A5,E2─┐
+		//   │      │       │
+		// A3,F3  A2,G3   A1,H3
+		//
+		{
+			"tree",
+			&Error{
+				error: &Error{
+					error: errors.Join(
+						&Error{
+							error: &Error{meta: map[string]any{"A": 3, "F": 3}},
+							meta:  map[string]any{"A": 4, "D": 2},
+						},
+						&Error{
+							error: errors.Join(
+								&Error{meta: map[string]any{"A": 2, "G": 3}},
+								&Error{meta: map[string]any{"A": 1, "H": 3}},
+							),
+							meta: map[string]any{"A": 5, "E": 2},
+						},
+					),
+					meta: map[string]any{"A": 6, "C": 1},
+				},
+				meta: map[string]any{"A": 7, "B": 0},
+			},
+			map[string]any{
+				"A": 7,
+				"B": 0,
+				"C": 1,
+				"D": 2,
+				"E": 2,
+				"F": 3,
+				"G": 3,
+				"H": 3,
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.testN, func(t *testing.T) {
+			// --- When ---
+			have := GetMeta(tc.err)
+
+			// --- Then ---
+			assert.Equal(t, tc.want, have)
+		})
+	}
+}
